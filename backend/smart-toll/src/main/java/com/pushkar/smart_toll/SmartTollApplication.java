790cf5d106607ec3a6b4e1e49cfe45212780d11d
Pushkkaarr <pushkarmmhatre3090@gmail.com>
2026-02-12 13:16:04 +0530

    feat: Initialize Smart Toll Navigator project with Maven setup and basic application structure, using Spring Boot

---
+++ backend/smart-toll/src/main/java/com/pushkar/smart_toll/SmartTollApplication.java
@@ -0,0 +1,13 @@
+package com.pushkar.smart_toll;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartTollApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartTollApplication.class, args);
+	}
+
+}
---
+++ backend/smart-toll/src/test/java/com/pushkar/smart_toll/SmartTollApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pushkar.smart_toll;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartTollApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
